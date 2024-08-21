c67e5e0845f67877c0154fec659c0df222818ab3
ARTHUR ALVES <contatoarthuralves@gmail.com>
2024-08-21 09:42:59 -0300

    Add project files.

---
+++ ConversorDolar/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConversorDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine("Informe o valor em dolar: ");
+            double valorEmDolar = double.Parse(Console.ReadLine());
+            double valorEmReais = valorEmDolar * 5.24;
+            Console.WriteLine("O valor em reais é: " + valorEmReais);
+            
+        }
+    }
+}
